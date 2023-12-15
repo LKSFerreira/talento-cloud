746acac3156f914ecc75cec8831300ad6e90111a
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-12-15 11:16:19 -0300

    :bug: Corrijdo atividade para demonstrar a iteração em ordem `crescente` e `decrescente`

--- introducao-a-programacao/code-park/desenvolvimento_3.py
+++ introducao-a-programacao/code-park/desenvolvimento_3.py
@@ -1,18 +1,30 @@
-# 1. Usando o laço de repetição `for`:
+# 1. Usando o laço de repetição `for` em ordem crescente:
 print()
+for andar in range(1, 21):
+    if andar == 13:
+        continue
+    print(andar)
+print("=============\n")
+
+# 2. Usando o laço de repetição `while` em ordem crescente:
+andar = 1
+while andar <= 20:
+    if andar != 13:
+        print(andar)
+    andar += 1
+print("=============\n")
+
+# 3. Usando o laço de repetição `for` em ordem decrescente:
 for andar in range(20, 0, -1):
     if andar == 13:
         continue
     print(andar)
 print("=============\n")
 
-# 2. Usando o laço de repetição `while`:
-
+# 4. Usando o laço de repetição `while` em ordem decrescente:
 andar = 20
 while andar > 0:
     if andar != 13:
         print(andar)
     andar -= 1
 print("=============\n")
-
-# Começando do 20º andar até o 1º andar (em ordem decrescente), pulando o 13º andar. A palavra-chave `continue` é usada para pular a iteração atual do laço e passar para a próxima. No caso do laço `while`, nós decrementamos o valor de `andar` em 1 a cada iteração.
