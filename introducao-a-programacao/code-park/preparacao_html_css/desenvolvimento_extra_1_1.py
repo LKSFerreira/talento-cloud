4bbf0e052920f8f1bfb9891726e5ee31d06c1ce8
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-12-23 12:38:57 -0300

    :sparkles: Atividade extra desenvolvimento 1.1
    :one: Preraração para HTML e CSS

---
+++ introducao-a-programacao/code-park/preparacao_html_css/desenvolvimento_extra_1_1.py
@@ -0,0 +1,8 @@
+print()
+# Array com produtos de uma loja de música
+produtos_loja_musica = ["Guitarra", "Bateria", "Baixo", "Teclado", "Microfone"]
+print("Produtos da loja de música:", produtos_loja_musica)
+
+# Array com anos de nascimento de familiares e amigos
+anos_nascimento = [1980, 1992, 2001, 1965, 1978]
+print("Anos de nascimento de familiares e amigos:", anos_nascimento)
