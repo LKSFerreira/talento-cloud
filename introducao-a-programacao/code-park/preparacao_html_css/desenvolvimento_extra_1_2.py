cf56efeb57c3fa40928558ce123e61706ba42d65
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-12-23 22:48:39 -0300

    :sparkles: Realizado exercício desenvolvimento extra 1.2

---
+++ introducao-a-programacao/code-park/preparacao_html_css/desenvolvimento_extra_1_2.py
@@ -0,0 +1,14 @@
+# Array com músicos
+lista_musicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+# Imprime a quantidade de elementos na lista
+print("Quantidade de elementos na lista:", len(lista_musicos))
+
+# Imprime o dado salvo no índice 2
+print("Dado no índice 2:", lista_musicos[2])
+
+# Imprime o dado salvo no índice 9
+print("Dado no índice 9:", lista_musicos[9])
+
+# Imprime o dado salvo no índice 14
+print("Dado no índice 14:", lista_musicos[14])
